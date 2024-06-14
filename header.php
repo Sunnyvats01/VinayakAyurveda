9f1b5b5bcea607f73abc5ab0c5630fccd8b7bac7
sunny01 <sunnyvats1201@gmail.com>
2024-06-14 11:16:39 +0530

    save

--- header.php
+++ header.php
@@ -2,21 +2,21 @@
           <img src="logo.jpg" alt="Logo" class="logo" />
   
           <ul class="nav-list">
-            <li><a href="index.html" class="nav-link">Home</a></li>
-            <li><a href="aboutUs.html" class="nav-link">About Us</a></li>
+            <li><a href="index.php" class="nav-link">Home</a></li>
+            <li><a href="aboutUs.php" class="nav-link">About Us</a></li>
             <li>
-              <a href="ourproducts.html" class="nav-link">Our Products</a>
+              <a href="ourproducts.php" class="nav-link">Our Products</a>
             </li>
             <li>
-              <a href="patentproduct.html" class="nav-link">Patent Products</a>
+              <a href="patentproduct.php" class="nav-link">Patent Products</a>
             </li>
             <li>
-              <a href="PCDpharma.html" class="nav-link">PCD Pharma Franchise</a>
+              <a href="PCDpharma.php" class="nav-link">PCD Pharma Franchise</a>
             </li>
             <li>
-              <a href="thirdparty.html" class="nav-link">Third Party Manufacuring</a>
+              <a href="thirdparty.php" class="nav-link">Third Party Manufacuring</a>
             </li>
-            <li><a href="ContactUs.html" class="nav-link">Contact Us</a></li>
+            <li><a href="ContactUs.php" class="nav-link">Contact Us</a></li>
             <!-- <li><i class="fa-solid fa-magnifying-glass fa-2xl"></i></li> -->
           </ul>
           <div class="burger">
@@ -27,18 +27,18 @@
   
           <div class="dropdown-menu">
             <ul >
-              <li><a href="index.html" class="nav-link">Home</a></li>
-              <li><a href="aboutUs.html" class="nav-link">About Us</a></li>
+              <li><a href="index.php" class="nav-link">Home</a></li>
+              <li><a href="aboutUs.php" class="nav-link">About Us</a></li>
               <li>
-                <a href="ourproducts.html" class="nav-link">Our Products</a>
+                <a href="ourproducts.php" class="nav-link">Our Products</a>
               </li>
               <li>
-                <a href="PCDpharma.html" class="nav-link">PCD Pharma Franchise</a>
+                <a href="PCDpharma.php" class="nav-link">PCD Pharma Franchise</a>
               </li>
               <li>
-                <a href="thirdparty.html" class="nav-link">Third Party Manufacuring</a>
+                <a href="thirdparty.php" class="nav-link">Third Party Manufacuring</a>
               </li>
-              <li><a href="ContactUs.html" class="nav-link">Contact Us</a></li>
+              <li><a href="ContactUs.php" class="nav-link">Contact Us</a></li>
             </ul>
           </div>
         </div>
